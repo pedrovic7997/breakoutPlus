1b99001ebcc13aed549f13bbb86dbeb3560c7533
Ryan Monteiro <37312658+rtmonteiro@users.noreply.github.com>
2021-09-05 19:48:27 -0300

    build: add bundler for ts (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import {Bola} from './Bola';
 
 const bola = new Bola(4, 4, 'red');
-
-const el = document.createElement('div');
-
-el.setAttribute('width', `${bola.width}`);
-el.setAttribute('height', `${bola.height}`);
-el.setAttribute('', `${bola.height}`);
-
-document.appendChild(el);
